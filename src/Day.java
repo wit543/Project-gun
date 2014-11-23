979e68de14b727416998c5fba2ecdee001541573
wit543 <norawit543@hotmail.com>
2014-11-23 19:01:26 +0700

    a

--- src/Day.java
+++ src/Day.java
@@ -1,4 +1,39 @@
 
 public class Day {
 
+	private int day;
+	private int month;
+	private int year;
+	
+	Day(int day,int month,int year){
+		this.day=day;
+		this.month=month;
+		this.year=year;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getDayTillTheMoviePermier(Day currentDay){
+		if((this.day-currentDay.day+(this.month-currentDay.month)*30+(this.year-currentDay.year)*365)>0)
+			return (this.day-currentDay.day+(this.month-currentDay.month)*30+(this.year-currentDay.year)*365);
+		return -1; // already show
+	}
+	public String toString(){
+		return day+"/"+month+"/"+year;
+	}
 }
--- src/l.java
+++ src/l.java
@@ -4,6 +4,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
 
 
 public class l extends JFrame {
@@ -34,8 +37,12 @@ public class l extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 	}
 
 }
